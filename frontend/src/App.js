1899b56cc46807acead3271faea28193d3b4530f
Tharun k <tharunmagai@gmail.com>
2026-01-26 15:05:40 +0530

    pipe lone triger

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ function App() {
   return (
     <div style={{textAlign:'center', marginTop:'50px'}}>
       <h1>Smart Campus Placement System hello how are you</h1>
-      <p>Frontend Running</p>
+      <p>Frontend Running by Tharun k </p>
     </div>
   );
 }
