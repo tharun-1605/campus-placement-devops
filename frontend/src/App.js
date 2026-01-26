a3ab78d688a007110f9dc466e9225a1ab211c293
Tharun k <tharunmagai@gmail.com>
2026-01-26 13:53:37 +0530

    update1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 function App() {
   return (
     <div style={{textAlign:'center', marginTop:'50px'}}>
-      <h1>Smart Campus Placement System</h1>
+      <h1>Smart Campus Placement System hello how are you</h1>
       <p>Frontend Running</p>
     </div>
   );
